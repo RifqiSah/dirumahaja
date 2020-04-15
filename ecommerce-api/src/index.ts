34b8bbc8bd12120cdecfc3157e25a9a7fd181c6a
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-15 21:44:05 +0700

    type: assign item route in index

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -1,39 +1,19 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
+import express, { Express } from 'express';
+import ItemRouter from './routes/item-routes';
 
-const app: Express = express();
+const app: Express = express();;
 const router = express.Router();
 
 // JSON parsing
 app.use(express.json());
 
-// Item interface
-interface Item {
-    id: number,
-    name: string,
-    price: number
-};
-
-// List of Items
-const items: Item[] = [];
-
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('<h1>Hello World!</h1>');
-});
-
 /**
  * Items API
  */
-router.get('/items', (req: Request, res: Response, next: NextFunction) => {
-    res.send(items);
-});
 
-router.post('/items', (req: Request, res: Response, next: NextFunction) => {
-    items.push(req.body);
-    res.send(req.body);
-});
+app.use('/items', ItemRouter);
 
 // Run!
-app.use(router);
 app.listen(5000, () => {
     console.log("Server running at port 5000!");
 });

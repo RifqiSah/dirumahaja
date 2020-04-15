4e0aaec7bfc411c286ff6dc7e1b5433a222bf21b
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-15 11:07:37 +0700

    type: add items API

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -3,16 +3,36 @@ import express, { Express, Request, Response, NextFunction } from 'express';
 const app: Express = express();
 const router = express.Router();
 
+// JSON parsing
+app.use(express.json());
+
+// Item interface
 interface Item {
     id: number,
     name: string,
     price: number
 };
 
+// List of Items
+const items: Item[] = [];
+
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('<h1>Hello World!</h1>');
 });
 
+/**
+ * Items API
+ */
+router.get('/items', (req: Request, res: Response, next: NextFunction) => {
+    res.send(items);
+});
+
+router.post('/items', (req: Request, res: Response, next: NextFunction) => {
+    items.push(req.body);
+    res.send(req.body);
+});
+
+// Run!
 app.use(router);
 app.listen(5000, () => {
     console.log("Server running at port 5000!");

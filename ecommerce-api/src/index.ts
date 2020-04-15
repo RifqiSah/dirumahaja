f4dca457c400ca846ca67fe09a9cda77e2cba655
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-15 11:02:39 +0700

    type: add item interface

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -3,6 +3,12 @@ import express, { Express, Request, Response, NextFunction } from 'express';
 const app: Express = express();
 const router = express.Router();
 
+interface Item {
+    id: number,
+    name: string,
+    price: number
+};
+
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('<h1>Hello World!</h1>');
 });

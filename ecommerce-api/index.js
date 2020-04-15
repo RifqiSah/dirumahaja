e0a784acbb92ed064968124a077ccd31ea579ebe
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-15 10:26:57 +0700

    type: add use before router

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -3,10 +3,19 @@ const app = express();
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello World!</h1>');
+    res.send(`<h1>Hello, ${req.user.name}!</h1>`);
 });
 
+app.use((req, res, next) => {
+    req.user = {
+        id: 1,
+        name: 'Rifqi'
+    };
+
+    next();
+});
 app.use(router);
+
 app.listen(5000, () => {
     console.log("Server running at port 5000!");
 });

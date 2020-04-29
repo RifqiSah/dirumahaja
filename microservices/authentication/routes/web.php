e040a4cb4ac7f53270a74c0b5b91abe3507b9ff2
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-30 05:26:17 +0700

    micro: auth endpoint

--- microservices/authentication/routes/web.php
+++ microservices/authentication/routes/web.php
@@ -14,3 +14,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/token', 'AuthController@login');
+$router->get('/me', 'AuthController@me');
